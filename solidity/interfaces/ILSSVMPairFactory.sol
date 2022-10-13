4d639c47810552c339661eba39efd7b216f49725
Weißer Hase <wei3erhase@defi.sucks>
2022-10-13 10:18:30 +0200

    feat: added Keep3r mechanism (#1)
    * feat: added keep3r scaffolding
    
    * feat: interfaces and improvements
    
    * fix: typo
    
    * feat: comments
    
    * feat: improved addliquidity method
    
    * feat: added e2e keep3r test environment

---
+++ solidity/interfaces/ILSSVMPairFactory.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: AGPL-3.0
+pragma solidity ^0.8.0;
+
+interface IERC721 {}
+
+interface ICurve {}
+
+interface LSSVMPair {
+    enum PoolType {SELL}
+}
+
+interface LSSVMPairETH is LSSVMPair {}
+
+interface ILSSVMPairFactory {
+    function createPairETH(
+        IERC721 _nft,
+        ICurve _bondingCurve,
+        address payable _assetRecipient,
+        LSSVMPair.PoolType _poolType,
+        uint128 _delta,
+        uint96 _fee,
+        uint128 _spotPrice,
+        uint256[] calldata _initialNFTIDs
+    ) external payable returns (LSSVMPairETH pair);
+}
--- solidity/test/e2e/Common.sol
+++ solidity/test/e2e/Common.sol
@@ -3,23 +3,44 @@ pragma solidity >=0.8.4 <0.9.0;
 
 import {DSTestFull} from 'test/utils/DSTestFull.sol';
 import {console} from 'forge-std/console.sol';
-import {IERC20} from 'isolmate/interfaces/tokens/IERC20.sol';
 
-import {Greeter, IGreeter} from 'contracts/Greeter.sol';
+import {ButtPlugWars, IKeep3r} from 'contracts/ButtPlugWars.sol';
+import {IERC20} from 'isolmate/interfaces/tokens/IERC20.sol';
+import {ERC721} from 'isolmate/tokens/ERC721.sol';
 
 contract CommonE2EBase is DSTestFull {
-    uint256 constant FORK_BLOCK = 15452788;
+    uint256 constant FORK_BLOCK = 15730000;
 
-    string initialGreeting = 'hola';
     address user = label('user');
     address owner = label('owner');
-    address daiWhale = 0x42f8CA49E88A8fd8F0bfA2C739e648468b8f9dec;
-    IERC20 dai = IERC20(0x6B175474E89094C44Da98b954EedeAC495271d0F);
-    IGreeter greeter;
+    ButtPlugWars buttPlugWars;
+    ERC721 fiveOutOfNine = ERC721(0xB543F9043b387cE5B3d1F0d916E42D8eA2eBA2E0);
+    IKeep3r keep3r = IKeep3r(0xeb02addCfD8B773A5FFA6B9d1FE99c566f8c44CC);
+    address constant KP3R_V1 = 0x1cEB5cB57C4D4E2b2433641b95Dd330A33185A44;
 
     function setUp() public {
         vm.createSelectFork(vm.rpcUrl('mainnet'), FORK_BLOCK);
-        vm.prank(owner);
-        greeter = new Greeter(initialGreeting, dai);
+        vm.startPrank(0xC5233C3b46C83ADEE1039D340094173f0f7c1EcF);
+
+        keep3r.bond(KP3R_V1, 0);
+        vm.warp(block.timestamp + 3 days + 1);
+        keep3r.activate(KP3R_V1);
+
+        buttPlugWars = new ButtPlugWarsForTest();
+    }
+}
+
+contract ButtPlugWarsForTest is ButtPlugWars {
+    function setState(STATE _state) external {
+        state = _state;
+    }
+
+    function internalUnbondLiquidity() external {
+        unbondLiquidity();
+    }
+
+    function workForTest() external {
+        IKeep3r(KEEP3R).isKeeper(0x99D281c0bf62B7062387C88D27e4D00669E748C6);
+        IKeep3r(KEEP3R).worked(0x99D281c0bf62B7062387C88D27e4D00669E748C6);
     }
 }
